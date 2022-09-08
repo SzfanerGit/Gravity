b7c1c93527988fae2f52323ccb133027b986cb4e
Kornel <kornel.ksiezak@gmail.com>
2022-09-08 15:17:28 +0200

    layout createed

--- app.py
+++ app.py
@@ -1,7 +1,19 @@
-from flask import Flask
+import os
 
+from flask import Flask, flash, redirect, render_template, request, session
+# from flask_session import Session
+from tempfile import mkdtemp
+from werkzeug.security import check_password_hash, generate_password_hash
+
+# Configure application
 app = Flask(__name__)
 
+# Ensure templates are auto-reloaded
+app.config["TEMPLATES_AUTO_RELOAD"] = True
+
 @app.route("/")
-def hello_world():
-    return "<p>Hello, World! lol</p>"
+def index():
+    return render_template("index.html")
+
+if __name__ == '__main__':
+    app.run(debug=True)
