54338a1e780534bebc6de44230c98cf0a1389a87
Kornel <kornel.ksiezak@gmail.com>
2022-09-08 19:25:16 +0200

    Register html works

--- app.py
+++ app.py
@@ -1,19 +1,52 @@
 import os
 
-from flask import Flask, flash, redirect, render_template, request, session
+from flask import Flask, url_for, flash, redirect, render_template, request, session
+from forms import RegistrationForm, LoginForm
 # from flask_session import Session
 from tempfile import mkdtemp
 from werkzeug.security import check_password_hash, generate_password_hash
 
 # Configure application
 app = Flask(__name__)
+app.config['SECRET_KEY'] = '8aabed13502bfd634e0f338428fe0e59'
 
 # Ensure templates are auto-reloaded
-app.config["TEMPLATES_AUTO_RELOAD"] = True
+app.config['TEMPLATES_AUTO_RELOAD'] = True
 
+@app.route("/index")
 @app.route("/")
 def index():
-    return render_template("index.html")
+    return render_template('index.html')
 
+
+@app.route("/register", methods=['GET', 'POST'])
+def register():
+    # Form submit via POST
+    if request.method == 'POST':
+        pass
+    
+    # User reaches via GET
+    form = RegistrationForm()
+    if form.validate_on_submit():
+        flash(f'Account for user: "{form.username.data}" sucessfully created!', 'sucess')
+        return redirect(url_for('index'))
+    return render_template('register.html', form=form)
+
+
+@app.route("/login", methods=['GET', 'POST'])
+def login():
+    # Form submit via POST
+    if request.method == 'POST':
+        pass
+    
+    # User reaches via GET
+    form = LoginForm()
+    return render_template('login.html', form=form)
+
+
+
+
+
+######################## Live changes for development ########################
 if __name__ == '__main__':
     app.run(debug=True)
---
+++ forms.py
@@ -0,0 +1,21 @@
+from flask_wtf import FlaskForm
+from wtforms import StringField, PasswordField, SubmitField, BooleanField
+from wtforms.validators import DataRequired, Length, Email, EqualTo
+
+
+class RegistrationForm(FlaskForm):
+    username = StringField('Username', validators=[DataRequired(), Length(min=3, max=30,
+                           message='Username must be between %(min)d and %(max)d characters long')])
+    email = StringField('Email', validators=[DataRequired(), Email()])
+    password = PasswordField('Password', validators=[DataRequired(), Length(min=8)])
+    password_confirm = PasswordField('Confirm Password', validators=[DataRequired(), EqualTo('password')])
+    submit = SubmitField('Sign Up!')
+
+
+class LoginForm(FlaskForm):
+    username = StringField('Username', validators=[DataRequired(), Length(min=3, max=30,
+                           message='Username must be between %(min)d and %(max)d characters long')])
+    password = PasswordField('Password', validators=[DataRequired(), Length(min=8,
+                             message='Password must be at least %(min)d characters long')])
+    remember = BooleanField('Remember Me')
+    submit = SubmitField('Login')
